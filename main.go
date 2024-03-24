e13ce68cb17b14a6fe9cae9c3a1e726173206566
RLungWu <larry14789@gmail.com>
2024-03-25 01:04:43 +0800

    Middleware Complete

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -16,30 +14,30 @@ type taskServer struct {
 	store *taskstore.TaskStore
 }
 
-func NewTaskServer() *taskServer{
+func NewTaskServer() *taskServer {
 	store := taskstore.New()
 	return &taskServer{store: store}
 }
 
-func (ts *taskServer) getAllTasksHandler(c *gin.Context){
+func (ts *taskServer) getAllTasksHandler(c *gin.Context) {
 	allTasks := ts.store.GetAllTasks()
 	c.JSON(http.StatusOK, allTasks)
 }
 
-func (ts *taskServer) deleteAllTasksHandler(c *gin.Context){
+func (ts *taskServer) deleteAllTasksHandler(c *gin.Context) {
 	ts.store.DeleteAllTasks()
 }
 
-func (ts *taskServer) createTaskHandler(c *gin.Context){
-	type RequestTask struct{
-		Text string `json:"text"`
-		Tags []string `json:"tags"`
-		Due time.Time `json:"due"`
+func (ts *taskServer) createTaskHandler(c *gin.Context) {
+	type RequestTask struct {
+		Text string    `json:"text"`
+		Tags []string  `json:"tags"`
+		Due  time.Time `json:"due"`
 	}
 
 	var rt RequestTask
-	if err := c.ShouldBindJSON(&rt); err != nil{
-		c.String(http.StatusBadRequest , err.Error())
+	if err := c.ShouldBindJSON(&rt); err != nil {
+		c.String(http.StatusBadRequest, err.Error())
 		return
 	}
 
@@ -47,15 +45,15 @@ func (ts *taskServer) createTaskHandler(c *gin.Context){
 	c.JSON(http.StatusOK, gin.H{"Id": id})
 }
 
-func (ts *taskServer) getTaskHandler(c *gin.Context){
+func (ts *taskServer) getTaskHandler(c *gin.Context) {
 	id, err := strconv.Atoi(c.Params.ByName("id"))
-	if err != nil{
+	if err != nil {
 		c.String(http.StatusBadRequest, err.Error())
 		return
 	}
 
 	task, err := ts.store.GetTask(id)
-	if err != nil{
+	if err != nil {
 		c.String(http.StatusNotFound, err.Error())
 		return
 	}
@@ -63,42 +61,42 @@ func (ts *taskServer) getTaskHandler(c *gin.Context){
 	c.JSON(http.StatusOK, task)
 }
 
-func (ts *taskServer) deleteTaskHandler(c *gin.Context){
-	id , err := strconv.Atoi(c.Params.ByName("id"))
-	if err != nil{
+func (ts *taskServer) deleteTaskHandler(c *gin.Context) {
+	id, err := strconv.Atoi(c.Params.ByName("id"))
+	if err != nil {
 		c.String(http.StatusBadRequest, err.Error())
 	}
 
-	if err = ts.store.DeleteTask(id); err != nil{
+	if err = ts.store.DeleteTask(id); err != nil {
 		c.String(http.StatusNotFound, err.Error())
 	}
 }
 
-func (ts *taskServer) tagHandler(c *gin.Context){
+func (ts *taskServer) tagHandler(c *gin.Context) {
 	tag := c.Params.ByName("tags")
 	tasks := ts.store.GetTasksByTag(tag)
 	c.JSON(http.StatusOK, tasks)
 }
 
-func (ts *taskServer) dueHandler(c *gin.Context){
-	badRequestError := func(){
+func (ts *taskServer) dueHandler(c *gin.Context) {
+	badRequestError := func() {
 		c.String(http.StatusBadRequest, "expect /due/<year>/<month>/<day>, got %v", c.FullPath())
 	}
 
 	year, err := strconv.Atoi(c.Params.ByName("year"))
-	if err != nil{
+	if err != nil {
 		badRequestError()
 		return
 	}
 
 	month, err := strconv.Atoi(c.Params.ByName("month"))
-	if err != nil{
+	if err != nil {
 		badRequestError()
 		return
 	}
 
 	day, err := strconv.Atoi(c.Params.ByName("day"))
-	if err != nil{
+	if err != nil {
 		badRequestError()
 		return
 	}
@@ -107,10 +105,11 @@ func (ts *taskServer) dueHandler(c *gin.Context){
 	c.JSON(http.StatusOK, tasks)
 }
 
-
-
-func main(){
+func main() {
 	router := gin.Default()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+	
 	server := NewTaskServer()
 
 	router.POST("/task/", server.createTaskHandler)
@@ -124,22 +123,3 @@ func main(){
 	router.Run("localhost:" + os.Getenv("SERVERPORT"))
 }
 
-
-
-/*
-func main(){
-	mux := http.NewServeMux()
-	server := NewTaskServer()
-
-	mux.HandleFunc("POST /task/", server.createTaskHandler)
-	mux.HandleFunc("GET /task", server.getAllTasksHandler)
-	mux.HandleFunc("GET /task/{id}", server.getTaskHandler)
-	mux.HandleFunc("DELETE /task/", server.deleteAllTasksHandler)
-	mux.HandleFunc("DELETE /task/{id}", server.deleteTaskHandler)
-	mux.HandleFunc("GET /tag/{tag}", server.tagHandler)
-	mux.HandleFunc("GET /due/{year}/{month}/{day}", server.dueHandler)
-
-	log.Fatal(http.ListenAndServe("localhost:"+os.Getenv("SERVERPORT"), mux))
-}
-
-*/
